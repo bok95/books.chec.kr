17a393a4ad4dea33ffe594f66c3d14b91fcbbc0e
Youngbok Yoon <youngbok.yoon@samsung.com>
2011-05-02 15:24:30 +0900

    Removed php tag in php files

--- books.php
+++ books.php
@@ -1,5 +1,9 @@
-<?
-
-print 'books';
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>books-checkr</title>
+	
+</head>
+<body>
+<span>books page</span>
+</body>
--- main.php
+++ main.php
@@ -1,4 +1,9 @@
-<?
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>book-checkr</title>
+	
+</head> 
+<body>
+<span>main page</span>
+</body>
