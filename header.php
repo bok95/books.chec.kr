5490bff63d1b16a5d745fe175b8a53dedb108e29
Youngbok Yoon <swcbok@gmail.com>
2011-07-13 17:35:58 +0900

    Merged new book page(designed by FLUX)

---
+++ header.php
@@ -0,0 +1,21 @@
+<div class="bar" id="header_bar" >
+</div>
+
+<div class="bar" id="main_nav" >
+	<ul id="main_menu" >
+	  <li class="menu_item" >Home</li>
+	  <li class="menu_item" >Most Downloaded</li>
+	  <li class="menu_item" >New Books</li>
+	  <li class="menu_item" >Book Apps</li>
+	</ul>
+</div>
+<div id="search_box" >
+	<div id="search_logo" >
+		
+	</div>
+		<form id="search_input_box" action="/" method="get" >
+			<input type="text" value="" id="search_input" name="q" />
+			<button id="search_btn" >
+		</button>
+		</form>
+</div>
